ecf92fd08bce4040fff82a4c09befc855dc45a85
reetah589 <reetah589@gmail.com>
2021-06-26 21:00:53 -0500

    folders and files added

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers/');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,5 +14,12 @@ app.use(routes);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 
